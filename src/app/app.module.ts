3aeea5f077b8b65ed1ed53b1c216a60e0c75bbf4
ArturSlasher <archiblablablapicher@gmail.com>
2021-09-05 23:02:22 +0300

    made layout sketch for login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,77 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
-import { LoginPageComponent } from './auth/login-page/login-page.component';
-import { LoginFormComponent } from './auth/login-page/login-form/login-form.component';
-import { AuthIllustrationComponent } from './auth/auth-illustration/auth-illustration.component';
-import { SignupPageComponent } from './auth/signup-page/signup-page.component';
-import { SignupFormComponent } from './auth/signup-page/signup-form/signup-form.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './home/header/header.component';
-import { HomeCardsComponent } from './home/map/home-cards/home-cards.component';
-import { NavtabsComponent } from './home/header/navtabs/navtabs.component';
-import { MyTripsComponent } from './home/my-trips/my-trips.component';
-import { TripsGroupComponent } from './home/my-trips/trips-group/trips-group.component';
-import { UserCardComponent } from './home/my-trips/trips-group/user-card/user-card.component';
-import { TripsListComponent } from './home/my-trips/trips-group/trips-list/trips-list.component';
-import { TripContentSectionComponent } from './home/my-trips/trip-places/trip-places.component';
-import { AuthComponent } from './auth/auth.component';
-import { MapComponent } from './home/map/map.component';
-import { AddTripComponent } from './home/map/add-trip/add-trip.component';
-import { AddPlaceComponent } from './home/my-trips/add-place/add-place.component';
-import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
-import { ComingSoonComponent } from './home/coming-soon/coming-soon.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AccountComponent } from './auth/account/account.component';
-import { AuthGuardService } from './services/auth-guard.service';
-import { EditAccountComponent } from './auth/account/edit-account/edit-account.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ImageDialogComponent } from './home/my-trips/trip-places/image-dialog/image-dialog.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginFormComponent } from './login-page/login-form/login-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    LoginFormComponent,
-    AuthIllustrationComponent,
-    SignupPageComponent,
-    SignupFormComponent,
-    HomeComponent,
-    HeaderComponent,
-    HomeCardsComponent,
-    NavtabsComponent,
-    MyTripsComponent,
-    TripsGroupComponent,
-    UserCardComponent,
-    TripsListComponent,
-    TripContentSectionComponent,
-    AuthComponent,
-    MapComponent,
-    AddTripComponent,
-    AddPlaceComponent,
-    ComingSoonComponent,
-    NotFoundComponent,
-    AccountComponent,
-    EditAccountComponent,
-    ImageDialogComponent
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    NgxMaterialTimepickerModule,
-    ReactiveFormsModule
+    MaterialModule
   ],
-  providers: [
-    AuthGuardService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,61 +1,18 @@
 import { NgModule } from "@angular/core";
-// import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
-import { MatButtonModule } from '@angular/material/button';
-// import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
-import { MatFormFieldModule } from "@angular/material/form-field";
-// import {MatLegacyCheckboxModule as MatCheckboxModule} from '@angular/material/legacy-checkbox';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
-// import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card';
-import { MatCardModule } from '@angular/material/card';
-// import {MatLegacyTabsModule as MatTabsModule} from '@angular/material/legacy-tabs';
-import { MatTabsModule } from '@angular/material/tabs';
-// import {MatLegacySliderModule as MatSliderModule} from '@angular/material/legacy-slider';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import { NgpImagePickerModule } from 'ngp-image-picker';
-// import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
-import { MatInputModule } from '@angular/material/input';
-// import {MatSidenavModule} from '@angular/material/sidenav';
-import { ReactiveFormsModule } from '@angular/forms';
-import {DialogModule} from '@angular/cdk/dialog';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatFormFieldModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatBadgeModule,
-    MatCardModule,
-    MatTabsModule,
-    MatBottomSheetModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    NgpImagePickerModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    DialogModule,
-    ScrollingModule
+    MatCheckboxModule
   ],
   exports: [
     MatButtonModule,
     MatFormFieldModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatBadgeModule,
-    MatCardModule,
-    MatTabsModule,
-    MatBottomSheetModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    NgpImagePickerModule,
-    MatInputModule,
-    DialogModule,
-    ScrollingModule
+    MatCheckboxModule
   ]
 })
 
