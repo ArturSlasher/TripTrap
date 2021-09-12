6e5de5c03d63e2084266d5cd956693ccf0737b65
ArturSlasher <archiblablablapicher@gmail.com>
2021-09-12 21:26:36 +0300

    created routing for auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, CanActivate } from '@angular/router';
-import { AuthGuardService } from './services/auth-guard.service';
-import { LoginPageComponent } from './auth/login-page/login-page.component';
-import { SignupPageComponent } from './auth/signup-page/signup-page.component';
-import { AccountComponent } from './auth/account/account.component';
-import { HomeComponent } from './home/home.component';
-import { ComingSoonComponent } from './home/coming-soon/coming-soon.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuardService]},
   { path: 'signup', component: SignupPageComponent},
   { path: 'login', component: LoginPageComponent},
-  { path: 'account', component: AccountComponent, canActivate: [AuthGuardService]},
-  { path: 'comming-soon', component: ComingSoonComponent},
-  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
