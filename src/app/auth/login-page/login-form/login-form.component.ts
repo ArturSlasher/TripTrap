3bd9bc62faad151e5c8c8b8b90ecb2119f11bd10
ArturSlasher <archiblablablapicher@gmail.com>
2022-07-12 20:31:41 +0300

    changed folders structure

--- src/app/auth/login-page/login-form/login-form.component.ts
+++ src/app/auth/login-page/login-form/login-form.component.ts
@@ -1,8 +1,4 @@
-import { TriptrapService } from 'src/app/services/triptrap.service';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { debounceTime, filter, map } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
@@ -11,76 +7,9 @@ import { Router } from '@angular/router';
 })
 export class LoginFormComponent implements OnInit {
 
-  @ViewChild("incorrect") incorrect!: ElementRef;
-  @ViewChild("incorrectEmail") incorrectEmail!: ElementRef;
-  @ViewChild("nonexistentEmail") nonexistentEmail!: ElementRef;
-  @ViewChild("incorrectPassword") incorrectPassword!: ElementRef;
-
-  loginForm: FormGroup = this.formBuilder.group({
-    emailControl:  [null,
-      [Validators.required, Validators.pattern(
-        /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$|demo/
-        )]],
-    passwordControl:  [null,
-      [
-        Validators.required,
-        Validators.pattern(
-        /^(?=.*\d).{4,8}$|demo/
-        )]]
-  })
-
-  constructor(
-    public triptrapService: TriptrapService,
-    public formBuilder: FormBuilder,
-    public router: Router
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.checkEmail();
-    this.checkPassword();
   }
 
-  checkEmail(){
-    this.loginForm.get('emailControl')?.valueChanges.pipe(
-      debounceTime(500),
-      filter(Boolean)
-      ).subscribe(value => {
-        if (this.loginForm.get('emailControl')?.valid){
-          this.incorrectEmail.nativeElement.style.display = 'none';
-          if (this.triptrapService.findUserByEmail(this.loginForm.get('emailControl')?.value)){
-            this.nonexistentEmail.nativeElement.style.display = 'none';
-          }
-          else {
-            this.nonexistentEmail.nativeElement.style.display = 'block';
-          }
-        }
-        else {
-          this.incorrectEmail.nativeElement.style.display = 'block';
-        }
-      });
-  }
-
-  checkPassword(){
-    this.loginForm.get('passwordControl')?.valueChanges.pipe(
-      debounceTime(500),
-      filter(Boolean)
-      ).subscribe(value => {
-        if (this.loginForm.get('passwordControl')?.valid){
-          this.incorrectPassword.nativeElement.style.display = 'none';
-        }
-        else {
-          this.incorrectPassword.nativeElement.style.display = 'block';
-        }
-      });
-  }
-
-  login(email: string, password: string) {
-    if (!this.loginForm.valid || !this.triptrapService.login(email, password)){
-      this.incorrect.nativeElement.style.display = 'block';
-    }
-    else {
-      this.triptrapService.login(email, password);
-      this.router.navigate(['']);
-    }
-  }
 }
