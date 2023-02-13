727f2391c568f7e3de99998bbad0b3ec426075ee
ArturSlasher <archiblablablapicher@gmail.com>
2023-02-13 22:24:53 +0200

    tried to make image viewer for places (2nd attempt)

--- src/app/home/my-trips/trip-places/trip-places.component.ts
+++ src/app/home/my-trips/trip-places/trip-places.component.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { TriptrapService } from 'src/app/services/triptrap.service';
 import { TripPlace } from 'src/app/interfaces/trip-place.interface';
 
+
 @Component({
   selector: 'app-trip-places',
   templateUrl: './trip-places.component.html',
@@ -11,31 +12,21 @@ import { TripPlace } from 'src/app/interfaces/trip-place.interface';
 })
 export class TripContentSectionComponent implements OnInit {
 
-
-
   CurrentTrip$ = this.TriptrapService.CurrentTrip$;
 
   constructor(
     private TriptrapService: TriptrapService
   ) {
-    // this.placeImage = document.querySelector("#test-image")!;
-    // this.fullImage = document.querySelector("#full-image")!;
-    // this.viewer = document.querySelector("#image-viewer")!;
-    // this.close = document.querySelector(".close")!;
-
     window.onload = () => {
-      const placeImage = document.querySelector("#test-image")!;
-      const fullImage = document.querySelector("#full-image")!;
-      const viewer = document.querySelector("#image-viewer")!;
-      const close = document.querySelector(".close")!;
-
-      console.log(placeImage)
+      const placeImage = document.getElementById("#test-image")!;
+      const fullImage = document.getElementById("#full-image")!;
+      const viewer = document.getElementById("#image-viewer")!;
+      const close = document.getElementById("#endregionclose")!;
 
       placeImage.onclick = () => {
         fullImage.setAttribute("src", "https://www.w3schools.com/howto/img_fjords.jpg");
         viewer.style.display = 'block';
       };
-
 
       // this.placeImage.attr("src")
       // https://www.w3schools.com/howto/img_fjords.jpg
@@ -50,8 +41,5 @@ export class TripContentSectionComponent implements OnInit {
   ngOnInit(): void {
 
   }
-
-
-
 
 }
