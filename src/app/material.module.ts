d8b2d43f915a577de93bed48e275569d32e607fb
ArturSlasher <archiblablablapicher@gmail.com>
2023-02-20 16:47:54 +0200

    fixed dependencies

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from "@angular/core";
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
-import {MatLegacyCheckboxModule as MatCheckboxModule} from '@angular/material/legacy-checkbox';
+// import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
+// import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
+import { MatFormFieldModule } from "@angular/material/form-field";
+// import {MatLegacyCheckboxModule as MatCheckboxModule} from '@angular/material/legacy-checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
-import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card';
-import {MatLegacyTabsModule as MatTabsModule} from '@angular/material/legacy-tabs';
-import {MatLegacySliderModule as MatSliderModule} from '@angular/material/legacy-slider';
+// import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card';
+import { MatCardModule } from '@angular/material/card';
+// import {MatLegacyTabsModule as MatTabsModule} from '@angular/material/legacy-tabs';
+import { MatTabsModule } from '@angular/material/tabs';
+// import {MatLegacySliderModule as MatSliderModule} from '@angular/material/legacy-slider';
 import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import { NgpImagePickerModule } from 'ngp-image-picker';
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
+// import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
+import { MatInputModule } from '@angular/material/input';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
@@ -23,7 +29,6 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatBadgeModule,
     MatCardModule,
     MatTabsModule,
-    MatSliderModule,
     MatBottomSheetModule,
     MatDatepickerModule,
     MatNativeDateModule,
@@ -39,7 +44,6 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatBadgeModule,
     MatCardModule,
     MatTabsModule,
-    MatSliderModule,
     MatBottomSheetModule,
     MatDatepickerModule,
     MatNativeDateModule,
