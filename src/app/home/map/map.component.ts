10f09fd898e4fad56d967054e0995d2b149687a2
ArturSlasher <archiblablablapicher@gmail.com>
2023-02-22 21:37:44 +0200

    added map api

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
 import { AddTripComponent } from './add-trip/add-trip.component';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
   constructor(private _bottomSheet: MatBottomSheet) { }
+
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
 
   openBottomSheet(): void {
     this._bottomSheet.open(AddTripComponent);
   }
 
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
   }
+
 
 }
 
