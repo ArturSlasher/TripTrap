c21b0b51b4abe687ccf1ced48e0913df937255df
ArturSlasher <archiblablablapicher@gmail.com>
2023-03-11 19:05:08 +0200

    made palces photos saving

--- src/app/home/my-trips/add-place/add-place.component.ts
+++ src/app/home/my-trips/add-place/add-place.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { ImagePickerConf } from 'ngp-image-picker';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { BehaviorSubject } from 'rxjs';
 import { TriptrapService } from 'src/app/services/triptrap.service';
 
 @Component({
@@ -16,6 +18,9 @@ export class AddPlaceComponent implements OnInit {
   @ViewChild("placeDate") placeDate!: ElementRef;
   @ViewChild("placeTime") placeTime!: ElementRef;
   @ViewChild("placeDescription") placeDescription!: ElementRef;
+  @ViewChild('addPhoto') addPhotoButton!: ElementRef;
+  @ViewChild('imagePicker', { static: false }) imagePicker: ElementRef | null | undefined = null;
+  photos: string[] = [];
 
   constructor(
     private triptrapService: TriptrapService,
@@ -23,6 +28,25 @@ export class AddPlaceComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  imagePickerConf: ImagePickerConf = {
+    borderRadius: '4px',
+    width: '30vw',
+    height: '20vw'
+  };
+
+  onImageChange(dataUri: any){
+    this.photos.push(dataUri);
+  }
+
+  imageCount$ = new BehaviorSubject<number[]>([1])
+  addExtraPhoto(){
+    // element.innerHTML = '<ngp-image-picker #imagePicker [_config]="imagePickerConf" ($imageChanged)="onImageChange($event)"></ngp-image-picker>';
+    const updatedImageCount = this.imageCount$.value;
+    this.imageCount$.value.push(this.imageCount$.value.length+1);
+    this.imageCount$.next(updatedImageCount);
+    console.log(this.imageCount$.value)
   }
 
   savePlace() {
@@ -33,7 +57,7 @@ export class AddPlaceComponent implements OnInit {
       date: this.placeDate.nativeElement.value,
       time: this.placeTime.nativeElement.value,
       description: this.placeDescription.nativeElement.value,
-      photos: []
+      photos: this.photos
     })
     this.CurrentTrip$.next(updatedCurrentTrip);
     this._bottomSheetRef.dismiss();
