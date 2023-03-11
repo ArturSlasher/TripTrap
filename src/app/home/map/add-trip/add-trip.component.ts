a762d3a224963bc452f079591858973791a966f0
ArturSlasher <archiblablablapicher@gmail.com>
2023-03-11 16:35:36 +0200

    made trip photo saving

--- src/app/home/map/add-trip/add-trip.component.ts
+++ src/app/home/map/add-trip/add-trip.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
 import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import { ImagePickerConf } from 'ngp-image-picker';
 import { Trip } from 'src/app/interfaces/trip.interface';
 import { TriptrapService } from 'src/app/services/triptrap.service';
 
@@ -8,23 +9,35 @@ import { TriptrapService } from 'src/app/services/triptrap.service';
   templateUrl: './add-trip.component.html',
   styleUrls: ['./add-trip.component.scss']
 })
-export class AddTripComponent implements AfterViewInit {
+export class AddTripComponent {
 
   Trips$ = this.triptrapService.Trips$;
   @ViewChild("tripName") tripName!: ElementRef;
   @ViewChild("tripStartDate") tripStartDate!: ElementRef;
   @ViewChild("tripEndDate") tripEndDate!: ElementRef;
+  @ViewChild('imagePicker', { static: false }) imagePicker: ElementRef | null | undefined = null;
 
   constructor(
     private triptrapService: TriptrapService,
     private _bottomSheetRef: MatBottomSheetRef
   ) {  }
 
+  imagePickerConf: ImagePickerConf = {
+    borderRadius: '4px',
+    width: '30vw',
+    height: '20vw'
+  };
+
+  imageSrc: string = '';
+  onImageChange(dataUri: any){
+    this.imageSrc = dataUri;
+  }
+
   saveTrip() {
     const newTrip: Trip = {
       name: this.tripName.nativeElement.value,
       date: this.tripStartDate.nativeElement.value + "-" + this.tripEndDate.nativeElement.value,
-      photo: "assets/trip-photo.png",
+      photo: this.imageSrc,
       tripPlaces: []
     }
     const updatedTrips: Trip[] = this.Trips$.value;
