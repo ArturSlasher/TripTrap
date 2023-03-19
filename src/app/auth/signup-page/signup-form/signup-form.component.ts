ca8dda7002c28bb480e89a18f79adec6ce6bbc14
ArturSlasher <archiblablablapicher@gmail.com>
2023-03-19 22:22:42 +0200

    added reactive forms in signup form

--- src/app/auth/signup-page/signup-form/signup-form.component.ts
+++ src/app/auth/signup-page/signup-form/signup-form.component.ts
@@ -1,6 +1,8 @@
 import { TriptrapService } from 'src/app/services/triptrap.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-signup-form',
@@ -10,29 +12,97 @@ import { Router } from '@angular/router';
 export class SignupFormComponent implements OnInit {
 
   @ViewChild("incorrect") incorrect!: ElementRef;
+  @ViewChild("incorrectEmail") incorrectEmail!: ElementRef;
+  @ViewChild("existentEmail") existentEmail!: ElementRef;
+  @ViewChild("incorrectPassword") incorrectPassword!: ElementRef;
+  @ViewChild("unconfimedPassword") unconfimedPassword!: ElementRef;
+
+  signupForm: FormGroup = this.formBuilder.group({
+    emailControl:  [null,
+      [Validators.required, Validators.pattern(
+        /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+        )]],
+    passwordControl:  [null,
+      [
+        Validators.required,
+        Validators.pattern(
+        /^(?=.*\d).{4,8}$/
+        )]],
+    confirmPasswordControl: [null],
+    usernameControl: [""]
+  })
 
   constructor(
     public triptrapService: TriptrapService,
+    public formBuilder: FormBuilder,
     public router: Router
   ) { }
 
+  ngOnInit(): void {
+    this.checkEmail();
+    this.checkPassword();
+    this.checkConfirmPassword();
+  }
+
+  checkEmail(){
+    this.signupForm.get('emailControl')?.valueChanges.pipe(
+      debounceTime(500),
+      filter(Boolean)
+      ).subscribe(value => {
+        console.log(this.signupForm.valid)
+        if (this.signupForm.get('emailControl')?.valid){
+          this.incorrectEmail.nativeElement.style.display = 'none';
+          if (!this.triptrapService.findUserByEmail(this.signupForm.get('emailControl')?.value)){
+            this.existentEmail.nativeElement.style.display = 'none';
+          }
+          else {
+            this.existentEmail.nativeElement.style.display = 'block';
+          }
+        }
+        else {
+          this.incorrectEmail.nativeElement.style.display = 'block';
+        }
+      });
+  }
+
+  checkPassword(){
+    this.signupForm.get('passwordControl')?.valueChanges.pipe(
+      debounceTime(500),
+      filter(Boolean)
+      ).subscribe(value => {
+        console.log(this.signupForm.valid)
+        if (this.signupForm.get('passwordControl')?.valid){
+          this.incorrectPassword.nativeElement.style.display = 'none';
+        }
+        else {
+          this.incorrectPassword.nativeElement.style.display = 'block';
+        }
+      });
+  }
+
+  checkConfirmPassword(){
+    this.signupForm.get('confirmPasswordControl')?.valueChanges.pipe(
+      debounceTime(500),
+      filter(Boolean)
+      ).subscribe(value => {
+        console.log(this.signupForm.valid)
+        if (this.signupForm.get('passwordControl')?.value === this.signupForm.get('confirmPasswordControl')?.value){
+          this.unconfimedPassword.nativeElement.style.display = 'none';
+        }
+        else {
+          this.unconfimedPassword.nativeElement.style.display = 'block';
+        }
+      });
+  }
+
   signup(email: string, password: string, confirmpassword: string, username: string){
-    if (email.length && username.length && this.checkPassword(password, confirmpassword)) {
-      this.triptrapService.signup(email, password, username);
-    }
-    else {
-      this.checkPassword(password, confirmpassword)
+    if (!this.signupForm.valid) {
       this.router.navigate(['signup']);
       this.incorrect.nativeElement.style.display = 'block';
     }
+    else {
+      this.triptrapService.signup(email, password, username);
+      this.router.navigate(['map']);
+    }
   }
-
-  checkPassword(password: string, confirmpassword: string) {
-    if (password.length >= 4 && password === confirmpassword) return true;
-    else return false;
-  }
-
-  ngOnInit(): void {
-  }
-
 }
